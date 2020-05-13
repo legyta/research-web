0a948538d8b010ee9f4027dac12bfd24364f47be
Caio Iglesias <caioiglesias@gmail.com>
2020-05-13 21:38:53 +0200

    Fix submit body

--- client/src/helper/responseHelper.js
+++ client/src/helper/responseHelper.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-export default async function sliderHelper(id, values) {
-  values.article_id = id;
-  const response = await axios.post("response", values, {
-    headers: { selectedArticleReducer: id },
-  });
+export default async function sliderHelper(values) {
+  const response = await axios.post("response", values);
   return response;
 }
